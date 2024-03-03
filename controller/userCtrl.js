0329caf77f990d9b88cd61784e5509f1269c1b97
Abdul Barik <abdulbarik1997m@gmail.com>
2024-03-04 00:55:11 +0600

    getallUser

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -28,8 +28,20 @@ const createUser = asyncHandler(async (req, res) => {
     }
   });
 
+  // Get all users
+
+const getallUser = asyncHandler(async (req, res) => {
+    try {
+      const getUsers = await User.find();
+      res.json(getUsers);
+    } catch (error) {
+      throw new Error(error);
+    }
+  });
+
 
   
 module.exports = {
-    createUser
+    createUser,
+    getallUser
   };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const {
-  createUser
+  createUser,
+  getallUser
 } = require("../controller/userCtrl");
 
 const router = express.Router();
+
+router.get("/all-users", getallUser);
 router.post("/register", createUser);
 
 
