754d3c2af1994c20e277eae56a94fa9de2ac6591
Abdul Barik <abdulbarik1997m@gmail.com>
2024-03-04 00:43:15 +0600

    User register

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const dbConnect = require("./config/dbConnect");
 const app = express();
 const dotenv = require("dotenv").config()
 
 const PORT = 5000;
+const authRouter = require("./routes/authRoute");
 
 dbConnect();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res)=>{ 
     res.status(200); 
     res.send("Welcome to root URL of Server"); 
 }); 
 
+app.use("/api/user", authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running  at PORT ${PORT}`);
